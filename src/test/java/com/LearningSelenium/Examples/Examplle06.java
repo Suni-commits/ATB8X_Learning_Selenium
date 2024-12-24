6c707e7faebf1caf2dfe72e79f22051ca8896df1
Suneetha <suneetha.la@gmail.com>
2024-12-24 09:54:35 +0530

    Updated 24th Dec

---
+++ src/test/java/com/LearningSelenium/Examples/Examplle06.java
@@ -0,0 +1,57 @@
+package com.LearningSelenium.Examples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class Examplle06 {
+
+    @Test
+    public void test_formMethods() throws InterruptedException {
+        ChromeOptions options=new ChromeOptions();
+
+        options.addArguments("--incognito");
+
+        options.addArguments("--start-maximized");
+
+        WebDriver driver=new ChromeDriver(options);
+        driver.get("https://awesomeqa.com/practice.html");
+
+        // Web Elements
+
+        WebElement firstname= driver.findElement(By.name("firstname"));
+        firstname.sendKeys("SUNEETHA");
+
+        WebElement lastname=driver.findElement(By.name("lastname"));
+        lastname.sendKeys("ILA");
+
+        WebElement gender=driver.findElement(By.id("sex-1"));
+        gender.click();
+
+        WebElement YearsofExperience=driver.findElement(By.id("exp-6"));
+        YearsofExperience.click();
+
+        WebElement date=driver.findElement(By.id("datepicker"));
+        date.sendKeys("24/12/2025");
+
+        WebElement profession= driver.findElement(By.id("profession-0"));
+        profession.click();
+
+        WebElement profession1= driver.findElement(By.id("profession-1"));
+        profession1.click();
+
+     WebElement selectdropdown=driver.findElement(By.id("continents"));
+        Select select=new Select(selectdropdown);
+        select.selectByVisibleText("South America");
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+    }
+}
